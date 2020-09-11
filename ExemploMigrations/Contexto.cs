f48fead7b96c559e64ca2937bdcaaca63e08f17a
laizsayuri <69865590+laizsayuri@users.noreply.github.com>
2020-09-11 18:26:02 -0300

    commit inicial

---
+++ ExemploMigrations/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploMigrations
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public Contexto() : base("ExemploMigrations.Properties.Settings.exemplo_migrationsConnectionString")
+        {
+        }
+    }
+}
---
+++ ExemploMigrations/Migrations/202009112025355_adicao_campo_email.cs
@@ -0,0 +1,18 @@
+﻿namespace ExemploMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class adicao_campo_email : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Usuarios", "Email", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Usuarios", "Email");
+        }
+    }
+}
